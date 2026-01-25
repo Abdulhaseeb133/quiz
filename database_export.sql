a317ed2face9b402979460bf3bea4a08fed20b7d
abdulhaseeb133 <a39856309@gmail.com>
2026-01-25 14:49:36 +0500

    modified database file

--- database_export.sql
+++ database_export.sql
@@ -19,6 +19,11 @@
 -- Table structure for table `quiz_question`
 --
 
+CREATE DATABASE quiz_db;
+USE quiz_db;
+SOURCE quiz_db.sql;
+
+
 DROP TABLE IF EXISTS `quiz_question`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
