def3c10ba7c267cb5dfc475679fbb4b6f93b5638
ahsan5263 <ahsanqayumshaikh@gmail.com>
2026-01-31 14:08:32 +0500

    add timer and same changes

--- quiz.js
+++ quiz.js
@@ -3,11 +3,11 @@ var score = 0;
 var questions = [];
 
 function startquiz() {
-   // insertStudent();
+    // insertStudent();
     GetQuestions();
-  
-    
-    
+
+
+
 }
 
 
@@ -34,7 +34,7 @@ function insertStudent() {
         fetch("http://localhost:3000/students", requestOptions)
             .then((response) => response.text())
             .then((result) => {
-                console.log(result);                
+                console.log(result);
 
             })
             .catch((error) => console.error(error));
@@ -44,26 +44,26 @@ function insertStudent() {
     }
 }
 
-function GetQuestions(){
+function GetQuestions() {
     const requestOptions = {
         method: "GET",
         redirect: "follow"
     };
 
     fetch("http://localhost:3000/questions", requestOptions)
-    .then((response) => response.text())
-    .then((result) => {
-        console.log(result);
-        questions = JSON.parse(result);
-        document.getElementById("quiz-question").style.display = "block";
-        document.getElementById("start-quiz").style.display = "none";
-        ShowNextQuestion();
-    })
-    .catch((error) => console.error(error));
+        .then((response) => response.text())
+        .then((result) => {
+            console.log(result);
+            questions = JSON.parse(result);
+            document.getElementById("quiz-question").style.display = "block";
+            document.getElementById("start-quiz").style.display = "none";
+            ShowNextQuestion();
+        })
+        .catch((error) => console.error(error));
 
 }
 
-function ShowNextQuestion(){
+function ShowNextQuestion() {
 
     document.getElementById("question").innerText = questions[index].question;
 
@@ -73,21 +73,107 @@ function ShowNextQuestion(){
     document.getElementById("optD").innerText = questions[index].D;
 }
 
-function NextQuestion(){   
+function NextQuestion() {
     Score();
     index++;
     ShowNextQuestion();
 }
 
-function Score(){
+function Score() {
     const user_ans = document.querySelector('input[name="answer"]:checked').value;
-    if(user_ans == questions[index].correct_answer){
+    if (user_ans == questions[index].correct_answer) {
         score++;
     }
 }
 
-// when last question is display, then need a logic for -> 
+function startquiz() {
+    // insertStudent();
+    GetQuestions();
+    startTimer(); // Start timer when quiz starts
+}
 
-// next button not show
-// show a button to show score and  finish the quiz 
-//
+// ...existing code...
+
+var timerInterval;
+var timeLeft = 30;
+
+function startTimer() {
+    timeLeft = 30;
+    document.getElementById("timer").innerText = timeLeft + " sec";
+    timerInterval = setInterval(function () {
+        timeLeft--;
+        document.getElementById("timer").innerText = timeLeft + " sec";
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+            trigger();
+        }
+    }, 1000);
+}
+
+function trigger() {
+    alert("Time's up!");
+}
+
+function ShowNextQuestion() {
+    document.getElementById("question").innerText = questions[index].question;
+    document.getElementById("optA").innerText = questions[index].A;
+    document.getElementById("optB").innerText = questions[index].B;
+    document.getElementById("optC").innerText = questions[index].C;
+    document.getElementById("optD").innerText = questions[index].D;
+
+    // Show Next button only if not last question
+    const nextBtn = document.querySelector('input[value="Next"]');
+    if (index < questions.length - 1) {
+        nextBtn.style.display = "inline-block";
+    } else {
+        nextBtn.style.display = "none";
+    }
+}
+
+function NextQuestion() {
+    Score();
+    index++;
+    if (index < questions.length) {
+        ShowNextQuestion();
+    } else {
+        endQuiz();
+    }
+}
+
+function endQuiz() {
+    clearInterval(timerInterval); // Stop timer
+    document.getElementById("quiz-question").style.display = "none";
+    document.getElementById("timer").style.display = "none";
+    // Create result page
+    const resultDiv = document.createElement("div");
+    resultDiv.innerHTML = `
+        <h2>Quiz Finished!</h2>
+        <p>Your Score: ${score} / ${questions.length}</p>
+    `;
+    document.body.appendChild(resultDiv);
+}
+
+
+var index = 0;
+var score = 0;
+var questions = [];
+var userAnswers = []; // Track user answers
+
+function Score() {
+    const checked = document.querySelector('input[name="answer"]:checked');
+    let user_ans = "";
+    if (checked) {
+        user_ans = checked.value;
+    }
+    userAnswers[index] = user_ans; // Save user's answer
+    if (user_ans == questions[index].correct_answer) {
+        score++;
+    }
+}
+
+// ...existing code...
+
+function trigger() {
+    alert("Time's up!");
+    endQuiz();
+}
